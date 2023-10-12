cb039e98575dceee9bd17170d98504ac921fecf1
ubiratan viana <ubiratan.dev@gmail.com>
2023-10-12 15:19:20 -0300

    video funcionando perfeito

--- app-odonto/src/Components/VideoPlayer.js
+++ app-odonto/src/Components/VideoPlayer.js
@@ -1,14 +1,14 @@
 import React from "react"
 import ReactPlayer from 'react-player/lazy';
 import kaduGay from './Imagens/kaduGay.mp4' ;
-import sandroCurio from './Imagens/sandroCurio.mp4';
+
 import './Components.css';
 const VideoPlayer=()=>{
     return(<div className="video">
        
       <div className="playerWrapper">
           
-          <ReactPlayer  controls controlsList="nodownload" url={sandroCurio}
+          <ReactPlayer  controls controlsList="nodownload" url={kaduGay}
            config={{ file: { attributes: { controlsList: 'nodownload' } } }}width='100%' height='60%'/>
       </div>
     </div>)
